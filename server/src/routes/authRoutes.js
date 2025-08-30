8f92024cb7c172984daa84e56aa9cf446e61ad03
Azlan Khawar <azlankhawas@gmail.com>
2025-08-31 01:51:27 +0530

    added auth routes

---
+++ server/src/routes/authRoutes.js
@@ -0,0 +1,21 @@
+//import supabase client 
+const supabase = require('../config/database')
+//auth middleware
+const authenticate = async (req, res, next) => {
+    try{    
+        const token = req.headers.authorization.split(' ')[1];
+        if(!token){
+            return res.status(401).json({error: 'No token provided'})
+        }
+        const {data:{ user }, error } = await supabase.auth.getUser(token);
+        if(error){
+            return res.status(401).json({error: 'Invalid token'})
+        }
+        req.user = user;
+        next();
+    }catch(error){
+        return res.status(500).json({error: 'Authentication failed'})
+    }
+}
+
+export default {authenticate};
